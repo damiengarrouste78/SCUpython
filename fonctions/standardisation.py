9a7af6482f79cb4b65da0a92386057e08f2f041a
Damien Garrouste <41282291+damiengarrouste78@users.noreply.github.com>
2019-06-03 10:24:59 +0200

    Add files via upload

--- fonctions/standardisation.py
+++ fonctions/standardisation.py
@@ -9,3 +9,17 @@ def standardisation(x):
     x-=np.mean(x) # the -= means can be read as x = x- np.mean(x)
     x/=np.std(x)
     return x
+
+# test
+# La variable __name__ varie selon le module dans lequel on se trouve durant l'exécution du programme. 
+# Dans le module principal, sa valeur sera égale à __main__.
+# alors que si le pgm est appellé par import  alors elle ne vaut pas main et ainsi l'instruction suivante
+# ne sera pas effectué 
+    
+if __name__=="__main__":
+    # variable aléa de loi uniforme entre 0 et 1
+    tab=np.random.uniform(1,2,100)
+    standardisation(tab)
+    print("la moyenne est elle bien nulle ?{}".format(tab.mean()))
+    
+    
