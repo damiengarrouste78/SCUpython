6d26509259befd8bd4b3cb9e0ba6b19a5cd26c30
Damien Garrouste <41282291+damiengarrouste78@users.noreply.github.com>
2019-06-03 10:25:30 +0200

    Add files via upload

--- pgms_COURS/4.application_fonction_module.py
+++ pgms_COURS/4.application_fonction_module.py
@@ -16,7 +16,7 @@ sys.path
 # importation d'une fonction stockée en module
 from standardisation import standardisation
 #import standardisation as std
-
+# on voit que ce qui est dans le main ne s'exécute pas
 
 import os
 os.getcwd()
@@ -28,8 +28,22 @@ import pandas as pd
 iris = pd.read_json("iris.json")
 
 # axis=0 on raisonne par col, on prend toutes les lignes de chaq col
+
+# utilisation de la fonction , en entrée on attend 1 vecteur
+iris_norm_petal_length= standardisation(iris['petalLength'])
+# ma fonction ne marche sur plusieurs vecteurs, donc il faut que j'applique col apres col la fonction
 iris_norm = iris[['petalLength', 'petalWidth', 'sepalLength', 'sepalWidth']].apply(standardisation,axis=0)
 
 # par défaut axis=0, on voit que la moy =0
-iris_norm.mean(axis=0)
-iris_norm.mean()
+resultat=dict(iris_norm.mean(axis=0))
+#iris_norm.mean()
+
+
+
+#  en json
+import json
+# on sauvegarde le dictionnaire avec la librairie json
+with open("exemple.json", "w") as f:
+    json.dump(resultat, f)
+#cette ligne s'affiche dans le sdtout par défaut donc la console en exec batch
+print("exécution terminée")
